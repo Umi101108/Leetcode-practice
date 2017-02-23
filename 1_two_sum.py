5b7cf25d82757a834d40ae34bbca4e5fb00ee7c6
wangwei <408465808@qq.com>
2017-02-23 22:37:50 +0800

    1
    two_sum

---
+++ 1_two_sum.py
@@ -0,0 +1,22 @@
+# coding:utf8
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == target - nums[j] :
+                    return [i, j]
+
+nums = [2, 7, 11, 15]
+target = 9
+so = Solution()
+print so.twoSum(nums, target)

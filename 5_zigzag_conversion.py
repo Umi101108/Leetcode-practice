1228969c12a7ab5b5f17e4b4b8f5bcb3d7aab079
Umi101108 <408465808@qq.com>
2017-03-02 17:38:07 +0800

    5

---
+++ 5_zigzag_conversion.py
@@ -0,0 +1,25 @@
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+"""
+
+class Solution(object):
+	def convert(self, s, numRows):
+		l = [[]] * numRows
+		for i in range(numRows):
+			l[i].append(i)
+			
+		print l
+
+
+so = Solution()
+so.convert('1213',3)
+

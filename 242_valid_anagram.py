ed84f3a21139b4ed7a6e8c37de426e2933d41e00
Umi101108 <408465808@qq.com>
2017-07-17 17:39:20 +0800

    242

---
+++ 242_valid_anagram.py
@@ -0,0 +1,25 @@
+"""
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+"""
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+
+
+s = 'apple'
+t = 'appee'
+so = Solution()
+print so.isAnagram(s, t)

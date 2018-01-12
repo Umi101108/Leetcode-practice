25c3f157219c338700b7d7c4d1697ee33ca8c357
Umi101108 <408465808@qq.com>
2018-01-12 17:04:18 +0800

    regain

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -11,10 +11,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
+        for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] == target - nums[j] :
                     return [i, j]
+
 
 nums = [2, 7, 11, 15]
 target = 9
